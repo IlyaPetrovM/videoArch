0d97f7b51063b77fd15c8a2beeba14bca6bb4577
Ilya Petrov <ilya.petrov.m@gmail.com>
2019-07-02 17:26:06 +0300

    Update make_a_video_proxy.bat

--- make_a_video_proxy.bat
+++ make_a_video_proxy.bat
@@ -1,8 +1,10 @@
-﻿:: Данный батник конвертирует все видеозаписи из текущей папки и подпапок в файлы с небольшим битрейтом
+:: Данный батник конвертирует все видеозаписи из текущей папки и подпапок в файлы с небольшим битрейтом
 :: Внимание! Расположите указанный файл в папке с видео. Рядом расположите папку ffmpeg. Запустите данный файл.
 :: ffmpeg -hide_banner -h encoder=h264_nvenc | xclip -sel clip
+:: -pix_fmt yuv420p требуется для того чтобы видео могло воспроизводиться на капризных плеерах вроде WMP или QuickTime
+
 mkdir "progress"
 mkdir "ready"
 mkdir "source"
-FOR /F "usebackq delims==" %%i IN (`dir /B /A-D /OS`) DO move %%i "progress" && ffmpeg -y -i "progress\%%i" -b:v 256k -maxrate 512k -b:a 128k -s 426x240 -r 25 -c:v h264_nvenc -pix_fmt yuv420p -c:a copy "ready\%%i_240p.mp4" && move "ready\%%i_240p.mp4" "Z:\Video"  &&  move "progress\%%i" "source"
+FOR /F "usebackq delims==" %%i IN (`dir /B /A-D /OS`) DO move "%%i" "progress" && ffmpeg -y -i "progress\%%i" -b:v 256k -maxrate 512k -b:a 128k -s 426x240 -r 25 -c:v h264_nvenc -pix_fmt yuv420p -c:a copy "ready\%%i_240p.mp4" && move "ready\%%i_240p.mp4" "Z:\Video"  &&  move "progress\%%i" "source"
 pause
