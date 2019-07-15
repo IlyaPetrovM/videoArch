299a75dd23753026bd2229e21430b9d38ddef8cc
Ilya Petrov <ilya.petrov.m@gmail.com>
2019-07-15 13:14:34 +0700

    Исправлена ошибка с переносом bat файла

--- make_a_video_proxy.bat
+++ make_a_video_proxy.bat
@@ -5,6 +5,9 @@ mkdir "progress"
 mkdir "ready"
 mkdir "source"
 
-FOR /F "usebackq delims==" %%i IN (`dir /B /A-D /OS`) DO move "%%i" "progress" && ffmpeg -y -i "progress\%%i" -b:v 256k -maxrate 512k -s 426x240 -r 25 -c:v h264_nvenc -pix_fmt yuv420p "ready\%%~ni_240p.mp4" &&  move "progress\%%i" "source"  
-
+FOR /F "usebackq delims==" %%i IN (`dir /B /A-D /OS`) DO ( 
+IF NOT "%%~xi"==".bat" (
+move "%%i" "progress" && ^
+ffmpeg -y -i "progress\%%i" -b:v 512k -maxrate 1024k -vf "scale=-1:'min(360,ih)'" -r 25 -c:v h264_nvenc -pix_fmt yuv420p -c:a copy "ready\%%~ni_360p.mp4" && ^ 
+move "progress\%%i" "source"))
 pause
